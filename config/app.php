122ea893e77c8289a2918473a7f9622cd0c9175c
abdulbasit-dev <abdullbasit99salah@gmail.com>
2022-10-29 22:39:00 +0300

    comment service

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        // Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
